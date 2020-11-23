e3fbc46f3f131a428865a7252974a1d3736ccb2d
Wojciech Jarosz <jaroszw@gmail.com>
2020-11-23 15:08:06 +0100

    before local storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import BookContextProvider from "./contexts/BookContext";
 import NavBar from "./components/NavBar";
 import BookList from "./components/BookList";
+import NewBookForm from "./components/BookForm";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BookContextProvider>
         <NavBar />
         <BookList />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
